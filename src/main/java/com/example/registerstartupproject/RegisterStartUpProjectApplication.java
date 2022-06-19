d49bcc455c01937c972bf8f71cd46d36670cb681
Adam Wasylewicz <adwas33@wp.pl>
2022-06-19 17:29:32 +0200

    Added swagger to test and maintain app. Disabled spring security for upgrade app .

--- src/main/java/com/example/registerstartupproject/RegisterStartUpProjectApplication.java
+++ src/main/java/com/example/registerstartupproject/RegisterStartUpProjectApplication.java
@@ -4,8 +4,10 @@ import com.example.registerstartupproject.adminPanel.BeginUsersSeed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RegisterStartUpProjectApplication {
 
     public static void main(String[] args) {
